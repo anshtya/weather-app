e8aadb6de4b1e4137e6542f6e2bb9216907698fd
Ansh Tyagi <tyagiansh486@gmail.com>
2023-08-03 14:02:53 +0530

    renamed from FakeUserDataRepository to TestUserDataRepository

--- app/src/test/java/com/anshtya/weatherapp/TestUserDataRepository.kt
+++ app/src/test/java/com/anshtya/weatherapp/TestUserDataRepository.kt
@@ -4,13 +4,13 @@ import com.anshtya.weatherapp.domain.model.UserData
 import com.anshtya.weatherapp.domain.repository.UserDataRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 
-class FakeUserDataRepository: UserDataRepository {
+class TestUserDataRepository: UserDataRepository {
 
     private val _data = MutableStateFlow(UserData(true))
 
-    override val userData: Flow<UserData>
-        get() = _data
+    override val userData: Flow<UserData> = _data.asSharedFlow()
 
     override suspend fun setWeatherUnit(useCelsius: Boolean) {
         _data.emit(UserData(useCelsius))
