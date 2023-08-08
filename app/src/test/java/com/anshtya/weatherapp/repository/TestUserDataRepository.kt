febf19f414587e60eb3ac654be8dd85705f827f5
Ansh Tyagi <tyagiansh486@gmail.com>
2023-08-08 11:21:43 +0530

    migrate from SharedFlow to StateFlow

--- app/src/test/java/com/anshtya/weatherapp/repository/TestUserDataRepository.kt
+++ app/src/test/java/com/anshtya/weatherapp/repository/TestUserDataRepository.kt
@@ -3,16 +3,17 @@ package com.anshtya.weatherapp.repository
 import com.anshtya.weatherapp.domain.model.UserData
 import com.anshtya.weatherapp.domain.repository.UserDataRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class TestUserDataRepository: UserDataRepository {
 
-    private val _data = MutableSharedFlow<UserData>()
+    private val _data = MutableStateFlow(UserData(showCelsius = true))
 
-    override val userData: Flow<UserData> = _data.asSharedFlow()
+    override val userData: Flow<UserData> = _data.asStateFlow()
 
     override suspend fun setWeatherUnit(useCelsius: Boolean) {
-        _data.emit(UserData(useCelsius))
+        _data.update { UserData(useCelsius) }
     }
 }
